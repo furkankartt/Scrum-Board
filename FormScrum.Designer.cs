3fd39f5524705030aceef31c01a2aceee321285c
Furkan <49095790+furkankartt@users.noreply.github.com>
2019-03-30 17:42:22 +0300

    Add files via upload

---
+++ FormScrum.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Scrum
+{
+    partial class FormScrum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelHikaye = new System.Windows.Forms.Panel();
+            this.panelBaslanacak = new System.Windows.Forms.Panel();
+            this.panelDevamEden = new System.Windows.Forms.Panel();
+            this.panelBiten = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonHikayeEkle = new System.Windows.Forms.Button();
+            this.buttonGorevEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelHikaye
+            // 
+            this.panelHikaye.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelHikaye.Location = new System.Drawing.Point(44, 47);
+            this.panelHikaye.Name = "panelHikaye";
+            this.panelHikaye.Size = new System.Drawing.Size(183, 314);
+            this.panelHikaye.TabIndex = 0;
+            // 
+            // panelBaslanacak
+            // 
+            this.panelBaslanacak.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBaslanacak.Location = new System.Drawing.Point(254, 47);
+            this.panelBaslanacak.Name = "panelBaslanacak";
+            this.panelBaslanacak.Size = new System.Drawing.Size(183, 314);
+            this.panelBaslanacak.TabIndex = 1;
+            // 
+            // panelDevamEden
+            // 
+            this.panelDevamEden.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDevamEden.Location = new System.Drawing.Point(464, 47);
+            this.panelDevamEden.Name = "panelDevamEden";
+            this.panelDevamEden.Size = new System.Drawing.Size(183, 314);
+            this.panelDevamEden.TabIndex = 1;
+            // 
+            // panelBiten
+            // 
+            this.panelBiten.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBiten.Location = new System.Drawing.Point(678, 47);
+            this.panelBiten.Name = "panelBiten";
+            this.panelBiten.Size = new System.Drawing.Size(183, 314);
+            this.panelBiten.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(102, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hikaye";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(294, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Başlanacak";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(509, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Devam Eden";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(745, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Biten";
+            // 
+            // buttonHikayeEkle
+            // 
+            this.buttonHikayeEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonHikayeEkle.Location = new System.Drawing.Point(82, 382);
+            this.buttonHikayeEkle.Name = "buttonHikayeEkle";
+            this.buttonHikayeEkle.Size = new System.Drawing.Size(92, 39);
+            this.buttonHikayeEkle.TabIndex = 0;
+            this.buttonHikayeEkle.Text = "Hikaye Ekle";
+            this.buttonHikayeEkle.UseVisualStyleBackColor = true;
+            this.buttonHikayeEkle.Click += new System.EventHandler(this.buttonHikayeEkle_Click);
+            // 
+            // buttonGorevEkle
+            // 
+            this.buttonGorevEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGorevEkle.Location = new System.Drawing.Point(293, 382);
+            this.buttonGorevEkle.Name = "buttonGorevEkle";
+            this.buttonGorevEkle.Size = new System.Drawing.Size(92, 39);
+            this.buttonGorevEkle.TabIndex = 1;
+            this.buttonGorevEkle.Text = "Görev Ekle";
+            this.buttonGorevEkle.UseVisualStyleBackColor = true;
+            this.buttonGorevEkle.Click += new System.EventHandler(this.buttonGorevEkle_Click);
+            // 
+            // FormScrum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 446);
+            this.Controls.Add(this.buttonGorevEkle);
+            this.Controls.Add(this.buttonHikayeEkle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelBiten);
+            this.Controls.Add(this.panelDevamEden);
+            this.Controls.Add(this.panelBaslanacak);
+            this.Controls.Add(this.panelHikaye);
+            this.Name = "FormScrum";
+            this.Text = "Scrum";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonHikayeEkle;
+        private System.Windows.Forms.Button buttonGorevEkle;
+        public System.Windows.Forms.Panel panelHikaye;
+        public System.Windows.Forms.Panel panelBaslanacak;
+        public System.Windows.Forms.Panel panelDevamEden;
+        public System.Windows.Forms.Panel panelBiten;
+    }
+}
+
---
+++ FormScrum.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrum
+{
+    public partial class FormScrum : Form
+    {
+        public FormHikayeEkle HikayeFormu = new FormHikayeEkle();
+        public FormGorevEkle GorevFormu = new FormGorevEkle();
+        public List<Hikaye> Hikayeler = new List<Hikaye>();
+        public List<Gorev> Gorevler = new List<Gorev>();
+
+        public FormScrum()
+        {
+            InitializeComponent();
+
+            HikayeFormu.Scrum = this;
+            GorevFormu.Scrum = this;
+        }
+        
+
+        private void buttonHikayeEkle_Click(object sender, EventArgs e)
+        {
+            HikayeFormu.ShowDialog();
+        }
+
+        private void buttonGorevEkle_Click(object sender, EventArgs e)
+        {
+            GorevFormu.ShowDialog();
+        }
+
+        public void HikayeGoruntule(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                foreach (Hikaye hky in Hikayeler)
+                {
+                    MessageBox.Show("Hikaye Başlığı: " + hky.HikayeBasligi + Environment.NewLine + "Hikaye Bitiş Süresi: " + hky.HikayeBitisSuresi 
+                        + Environment.NewLine + "Hikaye İçeriği: " + hky.HikayeIcerik);
+                }
+            }
+        }
+
+        public void GorevGoruntule(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                foreach (Gorev grv in Gorevler)
+                {
+                    MessageBox.Show("Hikaye Başlığı: " + grv.HikayeBasligi + Environment.NewLine + "Görev Kategori:  " + grv.GorevKategori
+                        + Environment.NewLine + "Görev İsmi: " + grv.GorevIsmi + Environment.NewLine + "Görev Sahibi: "+ grv.GorevSahibi + Environment.NewLine 
+                        + "Görev Tarihi : " + grv.GorevTarihi + Environment.NewLine + "Görev İçeriği: " + grv.GorevIcerik);
+                }
+            }
+        }
+
+        bool Durum = false;
+        Point IlkKonum;
+
+        public void GorevSurukleBirakMove (object sender, MouseEventArgs e)
+        {
+            Button ButonSec = (sender as Button);
+            if(Durum)
+            {
+                this.Controls.Add(ButonSec);
+                ButonSec.BringToFront();
+                ButonSec.Left = e.X + ButonSec.Left - (IlkKonum.X);
+                ButonSec.Top = e.Y + ButonSec.Top - (IlkKonum.Y);
+            }
+        }
+
+        public void GorevSurukleBirakDown(object sender, MouseEventArgs e)
+        {
+            Button ButonSec = (sender as Button);
+            Durum = true;
+            ButonSec.Cursor = Cursors.SizeAll;
+            IlkKonum = e.Location;
+        }
+
+        public void GorevSurukleBirakUp(object sender, MouseEventArgs e)
+        {
+            Button ButonSec = (sender as Button);
+            foreach (Gorev item in Gorevler)
+            {
+                Durum = false;
+                ButonSec.Cursor = Cursors.Default;
+                if((ButonSec.Location.X > panelBaslanacak.Location.X) && (ButonSec.Location.X < (panelBaslanacak.Location.X + panelBaslanacak.Width)) &&
+                    (ButonSec.Location.Y > panelBaslanacak.Location.Y) && (panelBaslanacak.Location.Y < (panelBaslanacak.Location.Y + panelBaslanacak.Height)) )
+                {
+                    item.GorevKategori = "Başlanacak";
+                    this.Controls.Remove(ButonSec);
+                }
+
+                else if ((ButonSec.Location.X > panelDevamEden.Location.X) && (ButonSec.Location.X < (panelDevamEden.Location.X + panelDevamEden.Width)) &&
+                    (ButonSec.Location.Y > panelDevamEden.Location.Y) && (panelDevamEden.Location.Y < (panelDevamEden.Location.Y + panelDevamEden.Height)))
+                {
+                    item.GorevKategori = "Devam Eden";
+                    this.Controls.Remove(ButonSec);
+                }
+
+                else if ((ButonSec.Location.X > panelBiten.Location.X) && (ButonSec.Location.X < (panelBiten.Location.X + panelBiten.Width)) &&
+                    (ButonSec.Location.Y > panelBiten.Location.Y) && (panelBiten.Location.Y < (panelBiten.Location.Y + panelBiten.Height)))
+                {
+                    item.GorevKategori = "Biten";
+                    this.Controls.Remove(ButonSec);
+                }
+
+                else
+                {
+                    this.Controls.Remove(ButonSec);
+                }
+            }
+            GorevFormu.GorevEkle();
+        }
+        
+    }
+}
